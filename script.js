fac2a89d3ce1ee4ccaadb9392ef2020ad1230f0d
bar <azariaba@gmail.com>
2017-08-19 02:47:04 +0300

    added navigation dots and the sixth slide

--- script.js
+++ script.js
@@ -10,28 +10,49 @@ function isEventSupported(eventName) {
     return isSupported;
 }
 
+var slideIndex = 1;
+
+function switch_to(index){
+    if(index == slideIndex)
+    {
+        return;
+    }
+    var div_to_scroll = document.getElementById('div_to_scroll');
+    var maxScrollLeft = div_to_scroll.scrollWidth - div_to_scroll.clientWidth;
+    var amountPerSlide = maxScrollLeft/5;
+    var newLocation = amountPerSlide*(index-1);
+    slideIndex = index;
+    $('#div_to_scroll').animate({scrollLeft : newLocation}, 1100);
+}
+
+
 $(document).ready(function() {
     var wheelEvent = isEventSupported('mousewheel') ? 'mousewheel' : 'wheel';
     $('body').on(wheelEvent, function(e) {
-        
         if($('#div_to_scroll').is(':animated'))
         {
             return false;
         }
         var oEvent = e.originalEvent,
             delta  = oEvent.deltaY || oEvent.wheelDelta;
-            var div_to_scroll = document.getElementById('div_to_scroll');
-            var maxScrollLeft = div_to_scroll.scrollWidth - div_to_scroll.clientWidth;
-            var quadScrollstr = String(maxScrollLeft/4);
-
-
-        if(delta>0)
+        var tempIndex = slideIndex;
+        if(delta > 0)
         {
-            $('#div_to_scroll').animate({scrollLeft : "+=" +quadScrollstr}, 1100);        
+            if(slideIndex < 6)
+            {
+                tempIndex++;
+            }
         }
-        else{
-            $('#div_to_scroll').animate({scrollLeft : "-=" + quadScrollstr}, 1100);    
+        else
+        {
+            if(slideIndex > 0)
+            {
+                tempIndex--;
+            }
         }
+        switch_to(tempIndex);
         e.preventDefault();
     });
+
+
 });
