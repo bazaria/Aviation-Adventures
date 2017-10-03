673af4d1f235b002208a01cbaaa288e35e2217bd
bar azaria <azariaba@gmail.com>
2017-10-03 05:45:27 +0300

    updated the mobile script

--- public/mobilescript.js
+++ public/mobilescript.js
@@ -13,29 +13,27 @@ function toggleslide (slidenum) {
 	var isOpen = (readmore.css('display') == 'none');
 
 	var slideheight = parseInt(slide.height());
-	var backgroundmargin = parseInt(nextbackground.css('top'));
 	
 	var animationduration = 300;
 	if(!isOpen){
-		var newslideheight = slideheight*1.2;
+		var newslideheight = slideheight*2;
 		console.log(slideheight);
 		console.log(newslideheight);
-		slide.animate({height: String(newslideheight) + 'vw'},animationduration);
-		for(i=slidenum + 1;i<7;i++){
-			$('#background' + String(i)).animate({top: String(newslideheight)  +'px'},animationduration);
-		}
+		slide.animate({height: String(newslideheight) + 'px'},animationduration);
+		$('#background' + nextstringnum).animate({marginTop: String(slideheight)  +'px'},animationduration,function(){
+					$('.content-wrapper').height($('body').height());
+			});
+
 		readmore.fadeToggle(animationduration/2, function(){
 			text.fadeToggle(animationduration/2);
 		});
 	}
 	else{
 		slide.animate({height: String('45.36vw')},animationduration);
-		for(i=slidenum + 1;i<7;i++){
-			$('#background' + String(i)).animate({top: '0px'},animationduration);
-		}
+			$('#background' + nextstringnum).animate({marginTop: '0px'},animationduration,function(){
+					$('.content-wrapper').height($('body').height());
+			});
 		text.fadeToggle(animationduration/2, function(){
 			readmore.fadeToggle(animationduration/2);
 		});
-	}
-	$('.content-wrapper').css('height','100%');
-}
+	}}
